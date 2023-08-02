5f7803e3155cd98c69ef8ef5f5ce7096dcf0d398
hakdogan <hakdogan75@gmail.com>
2023-08-02 21:17:00 +0300

    new methods added

--- util/src/main/java/org/jugistanbul/util/ThreadUtil.java
+++ util/src/main/java/org/jugistanbul/util/ThreadUtil.java
@@ -1,7 +1,10 @@
 package org.jugistanbul.util;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hakdogan (hakdogan75@gmail.com)
@@ -12,6 +15,14 @@ public class ThreadUtil
     public static void sleepOfSeconds(long duration){
         try {
             Thread.sleep(Duration.ofSeconds(duration));
+        } catch (InterruptedException ie){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void sleepOfMillis(long duration){
+        try {
+            Thread.sleep(Duration.ofMillis(duration));
         } catch (InterruptedException ie){
             Thread.currentThread().interrupt();
         }
@@ -28,5 +39,20 @@ public class ThreadUtil
 
     public static void joinAll(final List<Thread> threads){
         threads.forEach(ThreadUtil::join);
+    }
+
+    public static void wait(final ExecutorService executorService){
+
+        try {
+            executorService.shutdown();
+            executorService.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException ie){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static long benchmark(final Instant startTime){
+        var endTime = Instant.now();
+        return Duration.between(startTime, endTime).toMillis();
     }
 }
