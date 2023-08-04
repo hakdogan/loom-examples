135b56201756ea83dd58898bffac778c8f04937f
hakdogan <hakdogan75@gmail.com>
2023-08-04 23:52:27 +0300

    Monitoring pinning event

---
+++ virtual-threads/src/main/java/org/jugistanbul/virtualthread/monitor/MonitoringPinningEvent.java
@@ -0,0 +1,26 @@
+package org.jugistanbul.virtualthread.monitor;
+
+import org.jugistanbul.util.ThreadUtil;
+
+/**
+ * @author hakdogan (hakdogan75@gmail.com)
+ * Created on 4.08.2023
+ ***/
+public class MonitoringPinningEvent
+{
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        var thread = Thread.ofVirtual().unstarted(() -> {
+            //To see the pinning event, execute this class with runMonitoringPinningEvent.sh file
+            System.out.println("This virtual thread will be pinned on its carrier thread");
+            synchronized (lock){
+                ThreadUtil.sleepOfMillis(1);
+            }
+        });
+
+        thread.start();
+        ThreadUtil.join(thread);
+    }
+}
