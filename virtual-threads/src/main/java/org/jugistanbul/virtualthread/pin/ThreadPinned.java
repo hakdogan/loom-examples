de48249b052d6bdbb82e4ae76c258a444d73020d
hakdogan <hakdogan75@gmail.com>
2023-08-05 14:36:07 +0300

    Thread pinning

---
+++ virtual-threads/src/main/java/org/jugistanbul/virtualthread/pin/ThreadPinned.java
@@ -0,0 +1,37 @@
+package org.jugistanbul.virtualthread.pin;
+
+import org.jugistanbul.util.ThreadUtil;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author hakdogan (hakdogan75@gmail.com)
+ * Created on 5.08.2023
+ ***/
+public class ThreadPinned
+{
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        var threadList = IntStream.range(0, 10)
+                .mapToObj(i -> Thread.ofVirtual().unstarted(() -> {
+
+                    if (i == 0) {
+                        System.out.println(Thread.currentThread());
+                    }
+
+                    synchronized (lock) {
+                        ThreadUtil.sleepOfMillis(25);
+                    }
+
+                    if (i == 0) {
+                        System.out.println(Thread.currentThread());
+                    }
+
+                })).toList();
+
+        threadList.forEach(Thread::start);
+        ThreadUtil.joinAll(threadList);
+    }
+}
