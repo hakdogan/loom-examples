e012f104ed4a4c2ede7017ab5f66f843ce2bdac9
hakdogan <huseyin.akdogan@patikaglobal.com>
2024-04-16 23:30:11 +0300

    updated

--- structured-concurrency/runRequestProcessing.sh
+++ structured-concurrency/runRequestProcessing.sh
@@ -2,10 +2,11 @@
 
 #make sure the util and structured-concurrency modules are compiled before running this script
 
-java --enable-preview --source=22 \
+java --enable-preview --source 22 \
 -cp ../util/target/classes/:./target/classes \
-./src/main/java/org/jugistanbul/concurrency/structured/RequestProcessing.java & sleep 100 &
+./src/main/java/org/jugistanbul/concurrency/structured/RequestProcessing.java &
 for i in 1 2 3 4 5
 do
    curl -GET localhost:8080
+   sleep 1
 done
